358978605fd7fa0a79ac44da282f97df7ddbdc7e
Jinseo-Kim <kiy2344@naver.com>
2025-04-19 02:12:32 +0900

    new: day_receipt - make sturcture 	modified:   202533713_oil.py

--- 202533713_oil.py
+++ 202533713_oil.py
@@ -39,11 +39,11 @@ def date_setting(refresh = True):
         if refresh:
             day = str(int(day) + 1)
 
-        if 12 >= int(month) >= 1:
+        if 1 > int(month) > 12:
             print("caution: 잘못된 날짜입니다.")
             print(">>> 1~12월의 날짜를 입력해주세요.")
             init_date = input("> 날짜: ")
-        elif fixed_day[int(month)-1] >= int(day) >= 1:
+        elif  1 > int(day) > fixed_day[int(month)-1]:
             print("caution: 잘못된 날짜입니다.")
             print(">>> 해당 월에 알맞는 날짜를 입력해주세요.")
             init_date = input("> 날짜: ")
@@ -57,7 +57,7 @@ def date_setting(refresh = True):
 def oil_cost_update():
     oils = {}
     print("==================================================")
-    print(f'>> 오늘({year}{month}{day})의 유가 정보를 입력해주세요.')
+    print(f'>> 오늘({year}.{month}.{day})의 유가 정보를 입력해주세요.')
 
     while True:
         err_list = []
@@ -104,18 +104,29 @@ def main_screen():
 def week_receipt():
     print("주간 수입 내역서")
 
-def day_receipt():
-    print("일간 수입 내역서")
+def day_receipt(oil, liter, profit):
+    print("==================================================")
+    today = (".".join(map(str, [year, month, day])))
 
-def fuel_up():
+    if receipt.get(today, None) == None:
+        receipt[today] = [0, 0], [0, 0], [0, 0]
+    receipt[today][int(oil)-1][0] = liter
+    receipt[today][int(oil)-1][1] = profit
+    # print(f"> {today} 수입 내역서")
+    # print("------------------------------- ")
+    # print(f'1. {oils_info['1']}. {oils_info['1'][0]} {receipt[today][0][0]}L / {receipt[today][0][1]}원')
+    # print(f'2. {oils_info['2']}. {oils_info['2'][0]} {receipt[today][1][0]}L / {receipt[today][1][1]}원')
+    # print(f'3. {oils_info['3']}. {oils_info['3'][0]} {receipt[today][2][0]}L / {receipt[today][2][1]}원')
+    # print("-------------------------------")
+    # print(f"총 수입: {receipt[today][0][1] + receipt[today][1][1] + receipt[today][2][1]}원")
+
+def fuel_up(oils):
     print()
     print('{:=^40}'.format(" 주유 전 정전기 패드 터치!!! "))
     print('{:^40}'.format("< 금일 유가 >"))
 
-    cnt = 1
-    for key, value in oils_info.items():
-        print(f'{cnt}. {key}: {value}원 (1L당)')
-        cnt+=1
+    for key, value in oils.items():
+        print(f'{key}. {value[0]}: {value[1]}원/1L')
     print("--------------------------------------------------")
 
     oil_type = input("> 유종에 알맞는 번호를 입력해주세요: ")
@@ -125,18 +136,17 @@ def fuel_up():
         if error_chk(oil_type, 1) == False or oil_type not in ['1', '2', '3']:
             oil_type = input("> 유종에 알맞는 번호를 다시 입력해주세요: ")
             continue
-        if liter[0] != '0' and error_chk(liter):
+        if liter[0] == '0' and error_chk(liter):
             liter = input("> 주유하려는 기름의 양을 다시 입력해주세요(단위: L): ")
             continue
         break
 
-    profit = oils_info['1'][-1] * int(liter)
+    profit = int(oils[oil_type][-1]) * int(liter)
     print("===================================================")
-    print(f"> {oils_info[oil_type][0]} {liter}L 주유하였습니다.")
+    print(f"> {oils[oil_type][0]} {liter}L 주유하였습니다.")
     print(f"> 주유한 기름의 가격: {profit}원")
-    print("초기화면으로 돌아갑니다.")
-    print()
-    day_receipt()
+    print(">> 초기화면으로 돌아갑니다.")
+    day_receipt(oil_type, int(liter), profit)
 
 
 # 프로그램 실행
@@ -150,7 +160,7 @@ receipt = {}
 while True:
     select_menu = main_screen()
     if select_menu == '1':
-        fuel_up()
+        fuel_up(oils_info)
     elif select_menu == '2':
         print(">> 금일 마감이 완료되었습니다.")
         print(">> 날짜가 자동 갱신됩니다.")
