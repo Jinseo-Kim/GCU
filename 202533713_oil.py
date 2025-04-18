88192c0eed87ca26c19ea51c34939e42355b07fa
Jinseo-Kim <kiy2344@naver.com>
2025-04-18 14:43:33 +0900

    fix: init_date_setting() 	modified:   202533713_oil.py

--- 202533713_oil.py
+++ 202533713_oil.py
@@ -25,17 +25,41 @@ def error_chk(item, currect_length = False, with_msg = True):
     return True
 
 def init_date_setting():
+    fixed_day = ['31', '28', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31']
     print("==================================================")
     print(">> 프로그램이 정상적으로 실행되었습니다.")
     print(">> 오늘의 날짜를 입력해주세요.")
     print("(ex. 2025년 01월 01일 -> 250101)")
     init_date = input("> 날짜: ")
 
-    while error_chk(init_date, 6) == False:
-        init_date = input("> 날짜: ")
-    
+    while True:
+        if error_chk(init_date, 6) == False:
+            init_date = input("> 날짜: ")
+        year, month, day = init_date[0:2], init_date[2:4], init_date[4:]
+
+        if 12 >= int(month) >= 1:
+            print("caution: 잘못된 날짜입니다.")
+            print(">>> 1~12월의 날짜를 입력해주세요.")
+            init_date = input("> 날짜: ")
+        elif fixed_day[(month)-1] >= int(day) >= 1:
+            print("caution: 잘못된 날짜입니다.")
+            print(">>> 해당 월에 알맞는 날짜를 입력해주세요.")
+            init_date = input("> 날짜: ")
+        else:
+            break
+
     print()
-    return init_date
+    return year, month, day
+
+def date_update(yy, mm, dd):
+    print("==================================================")
+    mm, dd = int(mm), int(dd)+1
+    if dd > fixed_day[mm-1]:
+        dd = 1
+        mm += 1
+
+
+
 
 def oil_cost_update():
     oils = {}
@@ -44,15 +68,16 @@ def oil_cost_update():
 
     while True:
         err_list = []
-        oils['고급 휘발유'] = input("> 고급 휘발유(1L): ")
-        oils['보통 휘발유'] = input("> 보통 휘발유(1L): ")
-        oils['경유'] = input("> 경유(1L): ")
+        oils['1'] = ["고급 휘발유", input("> 고급 휘발유(1L): ")]
+        oils['2'] = ["보통 휘발유",input("> 보통 휘발유(1L): ")]
+        oils['3'] = ["경유",input("> 경유(1L): ")]
         
-        for key, value in oils.items():
-            chked_value = error_chk(value, with_msg = False)
+        for value in oils.values():
+            oil_type, price = value[0], value[1]
+            chked_value = error_chk(price, with_msg = False)
 
-            if value[0] == '0' or chked_value == False:
-                err_list.append(key)
+            if price[0] == '0' or chked_value == False:
+                err_list.append(oil_type)
 
         if err_list:
             print()
@@ -60,6 +85,8 @@ def oil_cost_update():
             print(">>> 유가 정보를 재입력해주세요.")
             print(f'>>> 잘못 입력된 값: {", ".join(err_list)}')
         else:
+            for chg_type_price in oils.values():
+                chg_type_price[1] = int(chg_type_price[1])
             break
 
     return oils
@@ -100,6 +127,7 @@ def fuel_up():
 
     oil_type = input("> 유종에 알맞는 번호를 입력해주세요: ")
     liter = input("> 주유하려는 기름의 양을 입력해주세요(단위: L): ")
+
     while True:
         if error_chk(oil_type, 1) == False or oil_type not in ['1', '2', '3']:
             oil_type = input("> 유종에 알맞는 번호를 다시 입력해주세요: ")
@@ -109,19 +137,17 @@ def fuel_up():
             continue
         break
 
-    profit = oils_info # 번호로 값을 받게되면 oils_info = {오일타입: 값}의 구조에서 oils_info = {1: [오일타입, 값], 2: [오일타입, 값], ...} 이런 형식으로 짜여야 함.
-            
-
-    success = input("> 주유 준비가 완료되었습니다. 주유 후 \'s\'를 입력해주세요: ")
+    profit = oils_info['1'][-1] * int(liter)
+    print("===================================================")
+    print(f"> {oils_info[oil_type][0]} {liter}L 주유하였습니다.")
+    print(f"> 주유한 기름의 가격: {profit}원")
+    print("초기화면으로 돌아갑니다.")
+    print()
     day_receipt()
-
-    print(">> 초기화면으로 돌아갑니다.")
-    
 
 
 # 프로그램 실행
-start_date = init_date_setting()
-year, month, day = start_date[0:2], start_date[2:4], start_date[4:]
+year, month, day = init_date_setting()
 oils_info = oil_cost_update()
 receipt = {}
 
@@ -130,9 +156,13 @@ while True:
     if select_menu == '1':
         fuel_up()
     elif select_menu == '2':
-        pass
+        print(">> 금일 마감이 완료되었습니다.")
+        print(">> 날짜가 자동 갱신됩니다.")
+        date_update(year, month, day)
+        oils_info = oil_cost_update()
+        print(">> 유가 정보가 갱신되었습니다.")
     elif select_menu == '3':
-        pass
+        week_receipt()
     elif select_menu == '4':
         print(">> 프로그램을 종료합니다.")
         exit()
