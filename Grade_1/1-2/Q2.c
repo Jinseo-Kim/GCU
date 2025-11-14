0c5dc84348f23415674a75c0c27f7c2ba53fea04
Jinseo-Kim <kiy2344@naver.com>
2025-11-14 16:56:03 +0900

    251104 * Q2 수정

--- Grade_1/1-2/Q2.c
+++ Grade_1/1-2/Q2.c
@@ -4,16 +4,32 @@ int main () {
     printf("202533713 김진서\n");
 
     int N, M, dan;
+    int i, j, k;
     int minSpace, avgSpace;
+    char space = 32;
 
     printf("N: ");
     scanf("%d", &N);
     printf("M: ");
-    scanf("%d", &N);
+    scanf("%d", &M);
 
     dan = M - N + 1;
     minSpace = 80 - (dan * 10) + dan + 1;
     avgSpace = (80 - (dan * 10)) / (dan + 1);
 
-    for ();
+    for (j = 1; j <= 9; j++)
+    {
+        for (i = 0; N + i <= M; i++)
+        {   
+            for (k = 0; k < avgSpace; k++)
+                printf("%c", space);
+
+            printf("%d x %d = %d", N + i, j, (N + i) * j);
+        }
+
+        for (k = 0; k < avgSpace; k++)
+            printf("%c", space);
+
+        printf("\n");
+    }
 }
